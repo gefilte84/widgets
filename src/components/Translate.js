3684c57eebc30c9c6bbd71c46b92addcf6e73b32
gefilte84 <58055602+gefilte84@users.noreply.github.com>
2020-09-30 15:42:01 +0200

    La til språk som vi kan oversette til

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,26 +1,40 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
 const options = [
 {
-    label: 'Afrikaans',
-    value: 'af',
+    label: 'Norwegian',
+    value: 'no',
 },
 {
     label: 'Arabic',
     value: 'ar',
 },
 {
-    label: 'Hindi',
-    value: 'hi',
+    label: 'Kurdish',
+    value: 'ku',
+},
+{
+    label: 'English',
+    value: 'en',
 }
 ];
-
+// Translate funksjonen med JSX
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value) } />
+                </div>
+            </div>
+            
             <Dropdown 
             label="Select a Language"
             selected={language}
